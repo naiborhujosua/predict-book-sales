5ee7e09d48d8cfc75f0a34d0a7ef8da14bacda57
N00b <naiborhujosua@users.noreply.github.com>
2022-09-23 09:19:49 +0700

    Update train.py

--- train.py
+++ train.py
@@ -19,7 +19,7 @@ warnings.simplefilter(action='ignore', category=Warning)
 # Load in the data
 df = pd.read_csv("predicting_num_sold.csv")
 
-Split into train and test sections
+# Split into train and test sections
 y = df.pop("num_sold")
 X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.2, random_state=seed)
 
